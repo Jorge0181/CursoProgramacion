b8eabb08dc4c373f14f131c415a366ef62fc0902
Jorge Agüero <jaguero.0181@gmail.com>
2023-02-10 14:09:57 -0300

    se agrego la funcion para simular el envio del formulario y se modifico la funcion infoDatos()

--- funcionesJava.js
+++ funcionesJava.js
@@ -1,36 +1,58 @@
-let idAnterior ="";
+let idAnterior = "";
 
-function aparecer (elemento) {
+/*function aparecer (elemento) {
     document.getElementById(elemento).style.display="block";    
-}
+}*/
 
+// Funcion que se usa para mostrar los datos personales en la seccion de informacion personal 
 function  infoDatos (ID) {
-    let ids = ["fechaNacimiento", "correo", "telefono", "domicilio"];
-    let info ="";
-
-    if(ID == "fechaNacimiento") {
-        info = "Fecha de nacimiento: 25/05/1810";
-    } else if (ID == "correo") {
-        info = "E-mail: darren@gmail.com";
-    } else if (ID == "telefono") {
-        info = "Telefono: (011) 555-5555";
-    } else if (ID == "domicilio") {
-        info = "Domicilio: Calle falsa 1234";
-    }
- /*   
-    for(let i=0; i<4; i++){        
-        document.getElementById(ids[i]+"Info").style.display = 'none';    
-    }
-  */  
+    let mensaje = {fechaNacimiento:"Fecha de nacimiento: 25/05/1810", correo:"E-mail: darren@gmail.com",telefono: "Telefono: (11) 6321-7777", domicilio:"Domicilio: Calle falsa 1234"};
+    let info = mensaje[ID];
+    console.log(info);
+ 
     if(idAnterior != ID){
         document.getElementById("informacion").style.display="flex";
-        document.getElementById("informacion").style.height = "40px"
+        document.getElementById("informacion").style.height = "40px";
         idAnterior = ID;
     } else {
         document.getElementById("informacion").style.display="none";
-        document.getElementById("informacion").style.height = "40px"
+        document.getElementById("informacion").style.height = "40px";
         idAnterior = "";
     }
     
     document.getElementById("informacion").innerHTML = info;
 }
+
+
+// Funcion que simula el envio del formulario
+document.addEventListener("click", function(e) {
+    let campo = ["nombre", "apellido", "email", "numTelefono", "mensaje"];
+    let mensaje = "";
+    let cont=document.getElementById("contenedor-mensaje");
+    let targ = e.target;
+    console.log(e);
+    
+    if(cont.style.display == "block" && targ != document.getElementById("btnEnviar")) {
+        console.log("oculta");
+        cont.style.display = "none";
+    } else if(targ == document.getElementById("btnEnviar")) {
+        for (let i=0; i<5; i++) {
+            if(document.getElementById(campo[i]).value == "") {
+                mensaje = "Debes completar el campo "+campo[i];
+                i=5;
+            } else {
+                mensaje = "El mensaje ha sido enviado exitosamente";
+            }
+        }
+
+        if(mensaje == "El mensaje ha sido enviado exitosamente") {
+            for (let i=0; i<5; i++) {
+                document.getElementById(campo[i]).value = " ";
+            }
+        }
+        
+        document.getElementById("mensajeForm").innerHTML = mensaje;
+        cont.style.display = "block";
+        console.log("se ve el div");      
+    }
+});
